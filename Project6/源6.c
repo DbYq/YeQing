376df7bcb74b332f7b22a2eb103b8c104b095874
DB <YeQingDinBin@yeah.net>
2020-10-25 00:16:10 +0800

    简单求大小与构造函数求大小

--- Project6/源6.c
+++ Project6/源6.c
@@ -1,33 +1,33 @@
-//比较两个数的大小
+//简单比较两个函数大小
 //#include<stdio.h>
 //int main()
 //{
 //	int x = 10;
 //	int y = 20;
-//	int z;
-//	if (x > y)z = x;
+//	int z = 0;
+//	if (x > y)
+//		z = x;
 //	else;
 //	z = y;
-//	printf("%d\n", z);
+//	printf("z=%d\n", z);
 //	return 0;
 //}
 
-//创建函数比较大小
+//构造一个函数来求最大值
 #include<stdio.h>
-int Max(int a, int b)
+int MAX(int a, int b)
 {
 	if (a > b)
 		return a;
 	else;
 	return b;
-
 }
 int main()
 {
-	int num1 = 10;
+	int num1=10;
 	int num2 = 20;
 	int max = 0;
-		max = Max(num1, num2);
+	max = MAX(num1, num2);
 	printf("max=%d\n", max);
 	return 0;
 }
