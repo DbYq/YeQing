cd5c9c753fd79a9c70f5348714aeb018b8bbe9a5
DB <YeQingDinBin@yeah.net>
2020-10-28 19:37:07 +0800

    上机训练1

---
+++ 上机训练/上机.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//		for (i = 1; i < 6; i++)
+//		{
+//			for (a = 1; a =2*i; a++);
+//		}
+//	return 0;
+//}
+
+//<2>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int num3 = 0;
+//	int max = 0;
+//	scanf("%d %d %d", &num1 ,& num2 ,& num3);
+//	if (num1 > num2 && num1 > num3)
+//	{
+//		max = num1;
+//		printf("max=%d\n", max);
+//	}
+//	if (num2 > num3 && num2 > num1)
+//	{
+//		max = num2;
+//		printf("max=%d\n", max);
+//	}
+//	if (num3 > num2 && num3 > num1)
+//	{
+//		max = num3;
+//		printf("max=%d\n", max);
+//	}
+//	return 0;
+//}
+//
+//<3>
