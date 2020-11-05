654a91db18d107e86fdc03a62a36cb781362ef63
DB <YeQingDinBin@yeah.net>
2020-11-05 22:42:43 +0800

    。。。。。。

---
+++ C语言程序设计题目/源1.c
@@ -0,0 +1,28 @@
+//第一题
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n % 3 == 0 && n % 5 == 0)
+//	{
+//		printf("该数可以被3和5整除\n");
+//	}
+//	else
+//	{
+//		printf("它不能被整除\n");
+//	}
+//	return 0;
+//}
+//第二题
+int main()
+{
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j < i; j++)
+
+	}
+}
