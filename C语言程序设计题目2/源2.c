54aaf02027935fded5487813d7bef0c95a8eed06
DB <YeQingDinBin@yeah.net>
2020-11-06 19:48:11 +0800

    求和

---
+++ C语言程序设计题目2/源2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 2; i <= 100; i++)
+	{
+		int sum = 1;
+		int z = 0;
+		if (0 == i % 2)
+		{
+			z = -i;
+		}
+	}
+}
